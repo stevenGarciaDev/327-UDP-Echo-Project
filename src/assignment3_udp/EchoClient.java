ffdd3d332fe1003306e5e431c5b6772ff5a0f56a
Steven Garcia <stevgarcia2015@gmail.com>
2020-04-24 13:34:48 -0700

    Add logic for EchoClient to prompt user

--- src/assignment3_udp/EchoClient.java
+++ src/assignment3_udp/EchoClient.java
@@ -1,5 +1,51 @@
 package assignment3_udp;
 
-public class EchoClient {
+import java.net.*;
+import java.util.*;
 
+public class EchoClient {
+	private DatagramSocket dsocket;
+	private DatagramPacket dpacket;
+	private InetAddress host; // IP address
+	private int serverPort;
+	private String message;
+	
+	public void createSocketConnection()
+	{
+		try {
+			dsocket = new DatagramSocket(this.serverPort, this.host);
+		} catch (SocketException e) {
+			System.out.println("Unable to create socket connection.");
+		}
+	}
+	
+	public void promptUser() {
+		Scanner scan = new Scanner(System.in);
+		boolean isInvalidInput = true;
+		
+		while (isInvalidInput) {
+		
+			try {
+				System.out.print("Please enter the IP address: ");
+				this.host = InetAddress.getByName(scan.nextLine());
+				
+				System.out.print("\n\nPlease enter the port number of the server: ");
+				this.serverPort = Integer.parseInt(scan.nextLine());
+				
+				System.out.print("\n\nPlease enter the message to be sent: ");
+				this.message = scan.nextLine();
+				
+				System.out.println("host " + this.host);
+				System.out.println("serverPort " + serverPort);
+				System.out.println("message " + message);
+				
+				isInvalidInput = false;
+			
+			} catch (Exception e) {
+				System.out.println("ERROR: Invalid input entered. Please try again.\n");
+			}
+		}
+		
+		scan.close();
+	}
 }
--- src/assignment3_udp/Test.java
+++ src/assignment3_udp/Test.java
@@ -1,7 +1,11 @@
 package assignment3_udp;
 
+import java.net.*;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		EchoClient client = new EchoClient();
+		client.promptUser();
+		EchoServer server = new EchoServer();
 	}
 }
